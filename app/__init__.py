bfe55e15277b1e3eaa76215be666f6565d20305c
kuailefengnan <kuailefengnan@kuailefengnandeMac-mini.local>
2019-02-13 10:09:33 +0800

    denglu

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 app = Flask(__name__)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 from app.admin import bp as admin_bp
 app.register_blueprint(admin_bp)
 from app.user import bp as user_bp
 app.register_blueprint(user_bp)
+
