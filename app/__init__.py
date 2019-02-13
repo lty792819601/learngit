f437a8b3a718d4a8a46fcba45b7bebb7015ce7e4
kuailefengnan <kuailefengnan@kuailefengnandeMac-mini.local>
2019-02-13 15:56:46 +0800

    同步

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 app = Flask(__name__)
+app.config.from_object
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
